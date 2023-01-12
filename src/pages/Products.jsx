079946f8124f2b328d6e3a95b968b946f6633b15
subin <whtnqlsm1004@gmail.com>
2023-01-12 17:53:45 +0900

    전체 상품 보기 페이지 작업

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ProductList from "../components/ProductList";
 
 function Products() {
-  return <></>;
+  return (
+    <>
+      <h2 className="text-2xl font-bold my-4 text-center">전체 상품</h2>
+      <ProductList />
+    </>
+  );
 }
 
 export default Products;
