7c4e76d21a8033e0db6008cc438596b21b6d9e48
subin <whtnqlsm1004@gmail.com>
2023-01-13 01:37:56 +0900

    결제 연동

---
+++ src/apis/iamport.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const useSetIamport = () => {
+  useEffect(() => {
+    const jquery = document.createElement("script");
+    jquery.src = "https://code.jquery.com/jquery-1.12.4.min.js";
+
+    const iamport = document.createElement("script");
+    iamport.src = "https://cdn.iamport.kr/js/iamport.payment-1.1.8.js";
+
+    document.head.appendChild(jquery);
+    document.head.appendChild(iamport);
+
+    return () => {
+      document.head.removeChild(jquery);
+      document.head.removeChild(iamport);
+    };
+  }, []);
+};
+
+export const useIamportPayment = () => {
+  const navigate = useNavigate();
+
+  useSetIamport();
+
+  const iamportPayment = (totalPrice) => {
+    const { IMP } = window;
+
+    IMP.init("imp28824362");
+
+    IMP.request_pay(
+      {
+        pg: "html5_inicis",
+        pay_method: "card",
+        merchant_uid: Date.now(),
+        name: "테스트 결제",
+        amount: totalPrice,
+        buyer_tel: "010-5248-7496",
+      },
+      (rsp) => {
+        if (rsp.success) {
+          navigate("/");
+        } else {
+          alert("결제에 실패했습니다. 다시 결제해 주세요.");
+        }
+      }
+    );
+  };
+
+  return {
+    iamportPayment,
+  };
+};
