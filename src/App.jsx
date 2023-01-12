de930bedab5408419ca92eba819d09b407b6092e
subin <whtnqlsm1004@gmail.com>
2023-01-12 16:57:17 +0900

    react-query 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <AuthContextProvider>
-      <Navbar />
-      <Outlet />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Navbar />
+        <Outlet />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
